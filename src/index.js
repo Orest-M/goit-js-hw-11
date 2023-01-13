fe95a6e57679d943af29e7ef78f5bfd2f4742809
Orest-M <orestmikhal@gmail.com>
2023-01-13 18:33:43 +0200

    add functions, pagination

--- src/index.js
+++ src/index.js
@@ -3,33 +3,115 @@ import Notiflix from 'notiflix';
 
 const form = document.querySelector('.search-form');
 const input = document.querySelector('.search-form input');
+const gallery = document.querySelector('.gallery');
+const loadMore = document.querySelector('.load-more');
 
 const URL = 'https://pixabay.com/api';
 const KEY = '32821640-07af0db556e394f2b39c0c0e4';
 
+let dataArray = null;
+let totalCount = null;
+let page = 1;
+
 form.addEventListener('submit', onSubmit);
+loadMore.addEventListener('click', onLoad);
 
 async function onSubmit(evt) {
   evt.preventDefault();
 
+  gallery.innerHTML = '';
+
   if (input.value.trim().length === 0) {
-    Notiflix.Notify.warning('Write something in the input field!');
+    callNotiflix('warn');
     return;
   }
 
-  const res = await axios.get(
-    `${URL}/?key=${KEY}&q=${input.value.trim()}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`
-  );
-  console.log(res.data);
+  try {
+    await axiosRequest();
 
-  const totalCount = res.data.totalHits;
+    if (totalCount === 0) {
+      callNotiflix('fail');
+      return;
+    }
 
-  if (totalCount === 0) {
-    Notiflix.Notify.failure(
+    callNotiflix('success');
+    createImageCard();
+
+    if (totalCount > 40) {
+      loadMore.style.display = 'block';
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function axiosRequest(page = 1) {
+  try {
+    const res = await axios.get(
+      `${URL}/?key=${KEY}&q=${input.value.trim()}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
+    );
+
+    dataArray = res.data.hits;
+    totalCount = res.data.totalHits;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function callNotiflix(status) {
+  if (status === 'success') {
+    return Notiflix.Notify.success(`Hooray! We found ${totalCount} images.`);
+  } else if (status === 'fail') {
+    return Notiflix.Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
-    return;
+  } else if (status === 'warn') {
+    return Notiflix.Notify.warning('Write something in the input field!');
   }
+}
 
-  Notiflix.Notify.success(`Hooray! We found ${totalCount} images.`);
+function createImageCard() {
+  console.log(dataArray);
+  const cards = dataArray
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<div class="photo-card" style="display: flex; flex-direction: column; align-items: center; border: 1px solid #363636; width: 370px">
+      <img src="${webformatURL}" alt="${tags}" loading="lazy" data-large="${largeImageURL}" width="100%" height="250px" />
+      <div class="info" style="display: flex; gap: 30px">
+        <p class="info-item" style="display: flex; flex-direction: column; align-items: center">
+          <b>Likes</b>
+          <span>${likes}</span>
+        </p>
+        <p class="info-item" style="display: flex; flex-direction: column; align-items: center">
+          <b>Views</b>
+          <span>${views}</span>
+        </p>
+        <p class="info-item" style="display: flex; flex-direction: column; align-items: center">
+          <b>Comments</b>
+          <span>${comments}</span>
+        </p>
+        <p class="info-item" style="display: flex; flex-direction: column; align-items: center">
+          <b>Downloads</b>
+          <span>${downloads}</span>
+        </p>
+      </div>
+    </div>`
+    )
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', cards);
+}
+
+function onLoad() {
+  page++;
+  axiosRequest(page);
+  createImageCard();
 }
