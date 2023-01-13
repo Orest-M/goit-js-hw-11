69fbcd6a38ccaf0b71669acb56d78a45030f25a6
Orest-M <orestmikhal@gmail.com>
2023-01-13 15:52:52 +0200

    install notiflix & axios. fetch to axios. add notiflix.notify

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
 const form = document.querySelector('.search-form');
 const input = document.querySelector('.search-form input');
-
-form.addEventListener('submit', onSubmit);
 
 const URL = 'https://pixabay.com/api';
 const KEY = '32821640-07af0db556e394f2b39c0c0e4';
 
+form.addEventListener('submit', onSubmit);
+
 async function onSubmit(evt) {
   evt.preventDefault();
 
-  const res = await fetch(
-    `${URL}/?key=${KEY}&q=${input.value}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`
-  );
-  const data = await res.json();
+  if (input.value.trim().length === 0) {
+    Notiflix.Notify.warning('Write something in the input field!');
+    return;
+  }
 
-  console.log(data);
+  const res = await axios.get(
+    `${URL}/?key=${KEY}&q=${input.value.trim()}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`
+  );
+  console.log(res.data);
+
+  const totalCount = res.data.totalHits;
+
+  if (totalCount === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+
+  Notiflix.Notify.success(`Hooray! We found ${totalCount} images.`);
 }
